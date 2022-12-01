0ab7b41e3058f1dffce336eaf74a573d1ac78929
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-12-01 13:27:05 +0500

    Add code

---
+++ CilBrowser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace CilBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodBase mb = MethodBase.GetCurrentMethod();
+            string html = HtmlGenerator.VisualizeMethod(mb);
+            File.WriteAllText("il.html", html);
+
+            Console.WriteLine("Generated!");
+            Console.ReadLine();
+        }
+    }
+}
