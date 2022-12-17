ad06ad167fef017306f5b738791b64ad7ea2c31d
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-12-17 15:44:58 +0500

    Support subdirectories
    Support recursing into dubdirectories when generating website from source directory

---
+++ CilBrowser.Core/NullClassifier.cs
@@ -0,0 +1,23 @@
+﻿/* CIL Browser (https://github.com/MSDN-WhiteKnight/CilBrowser)
+ * Copyright (c) 2022,  MSDN.WhiteKnight 
+ * License: BSD 3-Clause */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CilView.Core.Syntax;
+using CilView.SourceCode;
+
+namespace CilBrowser.Core
+{
+    class NullClassifier : TokenClassifier
+    {
+        internal static readonly NullClassifier Value = new NullClassifier();
+
+        private NullClassifier() { }
+
+        public override TokenKind GetKind(string token)
+        {
+            return TokenKind.Unknown;
+        }
+    }
+}
--- CilBrowser.Core/Utils.cs
+++ CilBrowser.Core/Utils.cs
@@ -3,6 +3,7 @@
  * License: BSD 3-Clause */
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace CilBrowser.Core
@@ -12,6 +13,18 @@ namespace CilBrowser.Core
         public static bool StrEquals(string left, string right)
         {
             return string.Equals(left, right, StringComparison.Ordinal);
+        }
+
+        public static string GetDirectoryNameFromPath(string path)
+        {
+            if (path.EndsWith("\\") || path.EndsWith("/"))
+            {
+                return Path.GetFileName(path.Substring(0, path.Length - 1));
+            }
+            else
+            {
+                return Path.GetFileName(path);
+            }
         }
     }
 }
