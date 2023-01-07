8d3218b141d843f145bff7b012ece93d63adfc31
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2023-01-07 16:14:22 +0500

    Add tests (#4)

---
+++ CilBrowser.Tests/SampleType.cs
@@ -0,0 +1,17 @@
+﻿/* CIL Browser (https://github.com/MSDN-WhiteKnight/CilBrowser)
+ * Copyright (c) 2023,  MSDN.WhiteKnight 
+ * License: BSD 3-Clause */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CilBrowser.Tests
+{
+    public class SampleType
+    {
+        public float X;
+        public float Y;
+
+        public float CalcSum() { return this.X + this.Y; }
+    }
+}
