347dd7134a194ee8742ad1743c1dd33a6e22f8cb
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2023-04-25 10:35:37 +0500

    Add files via upload

---
+++ CilBrowser.Core/SyntaxModel/FoxPro/FoxCommentToken.cs
@@ -0,0 +1,36 @@
+﻿/* CIL Browser (https://github.com/MSDN-WhiteKnight/CilBrowser)
+ * Copyright (c) 2023,  MSDN.WhiteKnight 
+ * License: BSD 3-Clause */
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using CilTools.Syntax.Tokens;
+
+namespace CilBrowser.Core.SyntaxModel.FoxPro
+{
+    class FoxCommentToken : SyntaxTokenDefinition
+    {
+        public override bool HasContinuation(string prevPart, TokenReader reader)
+        {
+            return !prevPart.EndsWith("\n");
+        }
+
+        public override bool HasStart(TokenReader reader)
+        {
+            string str = reader.PeekString(2);
+
+            if (Utils.StrEquals(str, "&&")) return true;
+
+            if (!str.StartsWith("*")) return false;
+
+            MethodInfo mi = typeof(TokenReader).GetMethod(
+                "GetPreviousChar", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            char prevChar = (char)mi.Invoke(reader, new object[] { 1 });
+
+            return prevChar == '\n' || prevChar == 0;
+        }
+    }
+}
+
